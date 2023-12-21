f761d40afd5a95b9ed69c08c0ef33c07a03665a4
Monalisa Octocat <octocat@github.com>
2023-12-21 12:17:45 +0200

    Update datastream id.

--- scripts/analytics/config-analytics.js
+++ scripts/analytics/config-analytics.js
@@ -1,18 +1,18 @@
 /*
  * Copyright 2023 Adobe. All rights reserved.
- * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * This file is licensed to you under the Apache License, Version 2.0 (the License);
  * you may not use this file except in compliance with the License. You may obtain a copy
  * of the License at http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software distributed under
- * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * the License is distributed on an AS IS BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
 
 const configs = {
   tenantId: '_dcbl',
-  edgeConfigId: '1f2639ad-325c-47cf-ad9b-45a406485aa8',
+  edgeConfigId: 'f46a95b1-547c-4614-a66b-2604778d8877',
   orgId: 'EA0C49475E8AE1870A494023@AdobeOrg',
 };
 
